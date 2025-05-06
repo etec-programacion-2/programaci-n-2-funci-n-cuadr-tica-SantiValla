89f158fc2ebd74156cd696c8a6d5e8bf13ae9726
SantiValla <s.valla@alumno.etec.um.edu.ar>
2025-05-06 08:44:48 -0300

    paso 1

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,16 @@
+
+package org.example
+//Santiago Valla
+
+fun main() {
+    
+    val a: Int = 2
+    val b: Int = 4
+    val c: Int = 6
+    if (a==0) {
+        println("justo ese valor de a no")
+    } else {
+        println("se va a hacer la siguiente funcion: $a x² + $b x + $c")
+    }
+}
+
